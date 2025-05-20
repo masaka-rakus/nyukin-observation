54fb4e7eec207602aadb8015d4d6eb5e58b1e486
masahiro.kawakami <masahiro.kawakami@rakus.co.jp>
2025-05-20 17:29:58 +0900

    [alloy] ot1へのpush用

--- run-alloy.sh
+++ run-alloy.sh
@@ -1,9 +1,9 @@
 docker run -d \
   --name alloy-toward-ot1 \
   -p 12345:12345 \
-  -v "/Users/masahiro.kawakami/repositories/nyukin-observation/logs:/logs" \
-  -v "/Users/masahiro.kawakami/repositories/nyukin-observation/alloy/config.alloy:/etc/alloy/config.alloy" \
-  -v "/Users/masahiro.kawakami/repositories/nyukin-observation/alloy/tk.domain.cer:/etc/ssl/certs/tk.domain.cer" \
+  -v "$(pwd)/logs:/logs" \
+  -v "$(pwd)/alloy/config.alloy:/etc/alloy/config.alloy" \
+  -v "$(pwd)/alloy/tk.domain.cer:/etc/ssl/certs/tk.domain.cer" \
   -v alloy-positions:/var/lib/alloy/positions \
   grafana/alloy:latest \
   run --server.http.listen-addr=0.0.0.0:12345 --storage.path=/var/lib/alloy/data /etc/alloy/config.alloy
